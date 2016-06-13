3e0ca50513d8a627cc06dd9cf4e4528ea069530d
Anton Bachin <antonbachin@yahoo.com>
2016-06-13 18:46:32 -0500

    0016dffbeb98c1da155865cd6b65504b85392b26
    Cygwin: setup.exe: don't create shortcuts

---
+++ cygwin/x86_64/ocaml/4.03/install.ps1
@@ -0,0 +1,114 @@
+$archive = "https://ci.appveyor.com/api/buildjobs/0fviw4pv42gve0qi/artifacts/packages-ocaml.zip"
+
+
+
+$arch = "x86_64"
+$inferred_package = "ocaml"
+$version = "4.03"
+
+
+
+# Error handling.
+$ErrorActionPreference = "Stop"
+
+function CheckExitCode {
+    if ($LastExitCode -ne 0) {
+        throw "$args exited with $LastExitCode"
+    }
+}
+
+# Cygwin.
+switch ($arch) {
+    "x86" {
+        $cygwin = "C:\Cygwin"
+        $setup = "C:\Cygwin\setup-x86.exe"
+        $bash = "C:\Cygwin\bin\bash.exe"
+    }
+    "x86_64" {
+        $cygwin = "C:\Cygwin64"
+        $setup = "C:\Cygwin64\setup-x86_64.exe"
+        $bash = "C:\Cygwin64\bin\bash.exe"
+    }
+    default {
+        throw "Unknown architecture $arch"
+    }
+}
+
+function Run($command) {
+    echo "+ $command"
+    iex $command
+}
+
+function Run-Bash {
+    $command = "$bash -lc '$args'"
+    Run $command
+    CheckExitCode $command
+}
+
+function Run-CygwinSetup {
+    rm $cygwin\var\log\setup*
+
+    $setup_args = @("-W", "-q", "-n") + $args
+
+    echo "+ cmd /c start /wait $setup $setup_args"
+    $code = {
+        param($setup, $setup_args)
+        cmd /c start /wait $setup $setup_args
+    }
+
+    if ("-L" -in $args) {
+        $timeout = 15
+    }
+    else {
+        $timeout = 30
+    }
+
+    $job = Start-Job $code -ArgumentList @($setup, $setup_args)
+    if (Wait-Job $job -Timeout $timeout) {
+        Receive-Job $job
+    }
+
+    if (Get-Process "setup-x86*" -ErrorAction SilentlyContinue) {
+        echo "WARNING: Setup is still running after $timeout s"
+    }
+
+    Remove-Job $job -Force
+    Stop-Process -Name "setup-x86*" -Force
+}
+
+# Make sure /etc/skel is copied now, to prevent surprising output that may be
+# captured later.
+Run-Bash "true"
+
+# Settle on a package name.
+if (-not (Test-Path variable:script:package)) {
+    $package = $inferred_package
+}
+
+# Working directory.
+$working_directory = "$env:TEMP\ocaml-binaries"
+
+if (Test-Path $working_directory) {
+    rm -recurse $working_directory
+}
+
+md $working_directory > $null
+
+# Package installation.
+function Install-Package {
+    $archive_name = "$working_directory\packages-$package.zip"
+    $packages_path = "$working_directory\packages-$package"
+
+    Run "Invoke-WebRequest '$archive' -OutFile '$archive_name'"
+    Run "Expand-Archive '$archive_name' '$packages_path'"
+
+    Run-CygwinSetup -L -l $packages_path -P $package
+}
+
+
+
+Run-CygwinSetup -P flexdll
+
+Install-Package
+
+Run-Bash "ocaml -version | grep $version"
